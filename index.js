18a842d730dc7b3e9800caed6f1b1bf79ccafbb1
Chelle77322 <serenynadarra@gmail.com>
2021-03-06 15:54:23 +1030

    Working on switching cases and SQL queries

--- index.js
+++ index.js
@@ -34,15 +34,103 @@ init();
   
 const startInquirer = require('./lib/inquirer');
 const inquirer = require('inquirer');
+const querySQL = require('./lib/querySQL');
 
 mainMenu();
  function mainMenu (){
    const menuChoices = new startInquirer(typeInquire[2],'selectChoice', userPrompt.choiceMenuPrompt, choices);
 
    inquirer.prompt([menuChoices.ask()]).then(operation =>{
-     console.log("You got here");
-   });
+  //Start of queries to bring all the information from each table: department, employees and role
+
+  const departmentQuery = "SELECT department.name FROM department"
+  const departArrayQuery = new querySQL(departmentQuery);
+
+  const roleQuery = "SELECT role.title FROM role"
+  const roleArrayQuery = new querySQL(roleQuery);
+  });
+  
  }
+ switch (operation.menuChoices) {
+
+  case menuChoices[2]:
+      //Will return all the employees
+      //return viewAllEmp();
+      console.log("This will be employees")
+
+  case menuChoices[3]:
+      //This is the case where a user can view all the employees in a given department
+      //queryReturnResult() is a method in my SQLqueries class that will run a query and return the result
+      // to the function delivered as the parameter
+      departArrayQuery.queryReturnResult(result);
+      break;
+
+  case menuChoices[4]:
+      //Employees under Manager
+   console.log("This will return all employees under their manager");
+break;
+  case menuChoices[5]:
+      //shows employees by their role
+      roleArrayQuery.getQueryNoRepeats(reuslt)
+      break;
+
+  case menuChoices[6]:
+      //This is the case where user can view all the managers and the departments they are in
+      //return viewAllManager();
+      console.log("managers will be here");
+      break;
+
+  case menuChoices[7]:
+      //This is the case for adding an employee
+     console.log("Add a department here");
+
+      break;
+
+  case menuChoices[8]:
+      //This is the case for deleting an employee
+      console.log('remove a department here');
+      break;
+
+  case menuChoices[9]:
+      //This is the case for the update an employees role funtion
+      console.log("Add a employee role here");
+
+      break;
+
+  case menuChoices[10]:
+      //This is the case for updating an employees manager
+     console.log("remove an employee role");
+     break;
+
+  case menuChoices[11]:
+      //Adds employee
+      console.log("Adds employee");
+     break;
+  case menuChoices[12]:
+      //This is the case for removing employee from database.
+     console.log("Removes an employee" )
+
+  case menuChoices[13]:
+      //updating employee role.
+     console.log("updating employee role");
+      break;
+  // return removeRole();
+
+  case menuChoices[0]:
+      //This is the case for viewing all the departments by name in the company
+     console.log("Time to view all departments");
+
+  case menuChoices[1]:
+      //View all departments
+      console.log("View all departments here");
+      break;
+
+  case menuChoices[14]:
+      //this is the case where user can delete a department from database
+      console.log("Updates the employee manager");
+      break;
+}
+
 
 
 
