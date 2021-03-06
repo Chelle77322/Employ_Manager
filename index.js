c940c5b4546d1556f9231ac5310ba1a1a6b4d2e4
Chelle77322 <serenynadarra@gmail.com>
2021-03-06 13:05:52 +1030

    SQL Connection working and logo printing

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
+//Declares the npm packages required for the scripts to work
+const logo = require('asciiart-logo');
 const mysql = require('mysql');
-const inquirer = require('inquirer');
+const {prompt}= require ('inquirer');
 const db = ("./db");
+const table = require('table');
 require('dotenv').config();
 
-//Makes the connection to the employment_database via the env
-const port = process.env.PORT;
-const address = process.env.SERVER_ADDRESS;
-
+//Accesses the env file to set the connection to the mySQL database schema
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.PORT,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    address: process.env.SERVER_ADDRESS
     
 });
+console.log ("You are connected to" + connection);
 
+//Accesses the modules for inquirer prompt
   const menuChoices = require('./lib/choices');
   const userPrompt = require ('./lib/userPrompt');
+init();
+  //Loads the ascii logo 
+  function init() {
+    const EMlogo = logo ({ name: "Employment Manager" }).render();
+  
+    console.log(EMlogo);
+  
+  }
   
     
 
