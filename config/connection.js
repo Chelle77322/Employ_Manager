8c0564e06e31346a97e752109f3bd0252ab9dd6d
Chelle77322 <serenynadarra@gmail.com>
2021-03-08 09:54:37 +1030

    Updating config and gitignore

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const mysql = require("mysql");
 const path = require("path");
-require('dotenv').config('../env');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') 
+})
 
 
+console.log(path);
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -10,7 +12,7 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
-console.log(connection);
+console.log(process.env['DB_USER']);
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
