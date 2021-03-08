2c0217550ed05dc50def13344fcac05327ccf0f6
Chelle77322 <serenynadarra@gmail.com>
2021-03-08 11:45:16 +1030

    Connection to database is fixed

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,33 @@
 const mysql = require("mysql");
 const util = require("util");
 const path = require("path");
-require('dotenv').config({path:__dirname + `/.env`});
+require('dotenv').config({path: __dirname +`/../env`});
 
 
+console.log(path);
 const connection = mysql.createConnection({
-  host: `${process.env.DB_HOST}`,
-  user: `${process.env.DB_USER}`,
-  password: `${process.env.DB_PASSWORD}`,
-  database: `${process.env.DB_NAME}`,
-  port : `${process.env.PORT}`
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port : process.env.PORT
 });
 
+console.log(process.env[`DB_HOST`]);
+console.log(process.env[`DB_USER`]);
+console.log(process.env[`DB_PASSWORD`]);
+console.log(process.env[`DB_NAME`]);
+console.log(process.env[`PORT`]);
+console.log(connection);
 
 
 connection.connect(function (err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+   console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
-   console.log("--------------------------------------");
+   //console.log("--------------------------------------");
 });
 
 module.exports = connection;
