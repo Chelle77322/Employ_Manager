28899cbb2ee1dbf928ebd0e67de50ccd9c41f507
Chelle77322 <serenynadarra@gmail.com>
2021-03-08 11:17:01 +1030

    trying to get rid of ENOENT error

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,26 @@
 const mysql = require("mysql");
+const util = require("util");
 const path = require("path");
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') 
-})
+require('dotenv').config({path:__dirname + `/.env`});
 
 
-console.log(path);
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: `${process.env.DB_HOST}`,
+  user: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_NAME}`,
+  port : `${process.env.PORT}`
 });
-console.log(process.env['DB_USER']);
+
+
+
 connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  // console.log("connected as id " + connection.threadId);
-  // console.log("--------------------------------------");
+  console.log("connected as id " + connection.threadId);
+   console.log("--------------------------------------");
 });
 
 module.exports = connection;
